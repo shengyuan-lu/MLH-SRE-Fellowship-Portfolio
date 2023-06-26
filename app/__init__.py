14c8296ddee051a7853daa00f53f9cc8337ab89a
Rami Elsayed <ramielsayed965@gmail.com>
2023-06-26 13:56:53 -0500

    Added Incomplete Style for Template

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/map')
+def map():
+    return render_template('map.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/about_me')
+def about_me():
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
