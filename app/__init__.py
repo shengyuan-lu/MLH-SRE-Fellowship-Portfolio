18322c3a7586ccc8f5273148b379246cec517f25
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-06-26 10:17:28 -0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,17 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+
 load_dotenv()
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
