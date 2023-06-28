022616dd1771a0167fc038a4011f41647ac4badc
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-06-28 00:40:14 -0700

    add education, work experience

--- app/__init__.py
+++ app/__init__.py
@@ -15,11 +15,33 @@ def index():
 
 @app.route('/shengyuan')
 def shengyuan():
-    return render_template('fellow.html', title="Fellow - Shengyuan Lu", url=os.getenv("URL"))
+    work_experiences = [
+        {'company': 'University of California', 'position': 'Software Engineering Intern', 'duration': 'July 2022 - Present'},
+        {'company': 'University of California', 'position': 'IT Support', 'duration': 'Sept 2021 - Dec 2021'},
+        {'company': 'Zoom Video Communications', 'position': 'Software Quality Assurance Intern', 'duration': 'July 2021 - Sept 2021'}
+    ]
+
+    education = [
+        {'institution': 'University of California, Irvine', 'degree': 'Bachelor of Science, Computer Science', 'year': '2020 - 2024'},
+        {'institution': 'Carroll High School', 'degree': 'High School Diploma', 'year': '2016 - 2020'},
+    ]
+
+    return render_template('fellow.html', title="Fellow - Shengyuan Lu", work_experiences=work_experiences, education=education, url=os.getenv("URL"))
 
 @app.route('/rami')
 def rami():
-    return render_template('fellow.html', title="Fellow - Rami Elsayed", url=os.getenv("URL"))
+
+    work_experiences = [
+        {'company': 'Company A', 'position': 'Position A', 'duration': '2010-2012'},
+        {'company': 'Company B', 'position': 'Position B', 'duration': '2012-2015'},
+        {'company': 'Company C', 'position': 'Position C', 'duration': '2015-2020'}
+    ]
+
+    education = [
+        {'institution': 'University of Wisconsin, Madison', 'degree': 'Bachelor of Science, Computer Science', 'year': '2022 - 2026'},
+    ]
+
+    return render_template('fellow.html', title="Fellow - Rami Elsayed",  work_experiences=work_experiences, education=education, url=os.getenv("URL"))
 
 if __name__ == '__main__':
     app.run(debug=True)
