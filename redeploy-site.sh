74ae74c4e7ff77d7ede479b63e67fc409f15a3de
Rami Elsayed <60044975+RamiE965@users.noreply.github.com>
2023-07-08 23:39:32 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 tmux kill-server
 git fetch && git reset --hard origin/main
-python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-# Start a new detached Tmux session and run the server
-tmux new-session -d -s redeploy 'source venv/bin/activate && flask run'
-
-
+export FLASK_ENV=development
+tmux new-session -d -s redeploy 'flask run'
