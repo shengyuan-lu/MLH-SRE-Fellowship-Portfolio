b3a8067bedc2500d82c9e257fca0ef0778aebf54
Rami Elsayed <ramielsayed965@gmail.com>
2023-07-14 19:31:43 -0500

    Fixed redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-tmux kill-server
+tmux kill-session
 git fetch && git reset --hard origin/main
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
 export FLASK_ENV=development
-tmux new-session -d -s redeploy 'flask run'
+tmux new-session -d -s redeploy 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
