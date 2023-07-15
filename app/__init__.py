f531ebbcf604859417b0f828db37e591dc14d784
Rami Elsayed <ramielsayed965@gmail.com>
2023-07-14 20:34:38 -0500

    Complete: Timeline Post API + Curl Test Script

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,66 @@
+import datetime
 import os
 from flask import Flask, render_template, request
+from playhouse.shortcuts import model_to_dict
 from dotenv import load_dotenv
+from peewee import *
 
 load_dotenv('example.env')
 
 app = Flask(__name__)
+print("URL:", os.getenv("URL"))
+print("Database:", os.getenv("MYSQL_DATABASE"))
+print("User:", os.getenv("MYSQL_USER"))
+print("Password:", os.getenv("MYSQL_PASSWORD"))
+print("Host:", os.getenv("MYSQL_HOST"))
+print("Port:", os.getenv("MYSQL_PORT"))
+
 app.config['GOOGLE_MAPS_API_KEY'] = os.getenv("google_maps_api_key")
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+              user=os.getenv("MYSQL_USER"),
+              password=os.getenv("MYSQL_PASSWORD"),
+              host=os.getenv("MYSQL_HOST"),
+              port=3306
+)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+@app.before_request
+def before_request():
+    mydb.connect()
+@app.before_first_request
+def create_tables():
+    with mydb:
+        mydb.create_tables([TimelinePost])
+print(mydb)
 
 @app.route('/')
 def index():
     return render_template('index.html', title="Meet MLH Fellows", url=os.getenv("URL"))
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
 
 @app.route('/shengyuan')
 def shengyuan():
