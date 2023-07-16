306eaae3fe6d458a773c66e7f15e66f0e5c92584
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-07-16 13:51:01 -0700

    finish Adding database - part 2

---
+++ curl-test.sh
@@ -0,0 +1,5 @@
+curl --request GET http://localhost:5000/api/timeline_post
+
+curl --request POST http://localhost:5000/api/timeline_post -d 'name=Shengyuan&email=shengyuan.lu.developer@gmail.com&content=test'
+
+curl --request GET http://localhost:5000/api/timeline_post
