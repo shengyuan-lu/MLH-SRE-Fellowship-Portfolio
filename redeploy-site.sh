41f275bdaf00b1cfc042954973aa7e7e6df15a50
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-08-04 14:28:24 -0700

    Using docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
+cd /root/mlh/mlh-sre-portfolio
 
-# Step 1: end service
-echo 'End myportfolio service'
-systemctl stop myportfolio
-
-# Step 2: Change directory to your project folder
-cd /root/mlh-sre-fellowship-portfolio
-
-# Step 3: Fetch and reset the Git repository to the latest changes from GitHub
 git fetch && git reset origin/main --hard
 
-# Step 4: Activate Python virtual environment and install dependencies
-echo 'Activate Python virtual environment'
-source /root/mlh-sre-fellowship-portfolio/python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-# Step 5: restart service
-echo 'Restart myportfolio service'
-systemctl daemon-related
-systemctl start myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
