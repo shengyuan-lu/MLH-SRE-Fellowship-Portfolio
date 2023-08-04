b4d6990fb6c4cd90e591fd4538d842b738b841bb
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2023-08-04 16:15:43 -0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,24 +2,39 @@
 
 import unittest
 import os
+from peewee import *
 
 os.environ['TESTING'] = 'True'
 
-from app import app
+from app import app, TimelinePost
+MODELS = [TimelinePost]
+
+test_db = SqliteDatabase(':memory:')
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         # Bind model classes to test db. Since we have a complete list of
         # all models, we do not need to recursively bind dependencies.
         self.client = app.test_client()
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+        test_db.connect()
+        test_db.create_tables(MODELS)
+
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live
+        # for the duration of the connection, and in the next step we close
+        # the connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
+        # Close connection to db.
+        test_db.close()
 
     def test_home(self):
         response = self.client.get("/")
         assert response.status_code == 200
         html = response.get_data(as_text=True)
         assert "<title>Meet MLH Fellows</title>" in html
-        assert "Meet MLH Fellows" in html  
-        assert "Shengyuan Lu" in html  
+        assert "Meet MLH Fellows" in html
+        assert "Shengyuan" in html
 
 
     def test_timeline(self):
@@ -32,14 +47,14 @@ class AppTestCase(unittest.TestCase):
 
         new_post = {"name": "John", "email": "john@example.com", "content": "Test Content"}
         post_response = self.client.post("/api/timeline_post", data=new_post)
-        assert post_response.status_code == 200  
+        assert post_response.status_code == 200
         response = self.client.get("/api/timeline_post")
         json = response.get_json()
-        assert len(json["timeline_posts"]) == 1 
+        assert len(json["timeline_posts"]) == 1
 
-        assert json["timeline_posts"][0]["name"] == "John"  
-        assert json["timeline_posts"][0]["email"] == "john@example.com" 
-        assert json["timeline_posts"][0]["content"] == "Test Content" 
+        assert json["timeline_posts"][0]["name"] == "John"
+        assert json["timeline_posts"][0]["email"] == "john@example.com"
+        assert json["timeline_posts"][0]["content"] == "Test Content"
 
 
     def test_malformed_timeline_post(self):
@@ -50,7 +65,7 @@ class AppTestCase(unittest.TestCase):
         assert "Invalid name" in html
 
         # POST request with empty content
-        response = self.client.post("/api/timeline_post", data= {"name": "John Doe", "email": "john@example.com", "content": ""}) 
+        response = self.client.post("/api/timeline_post", data= {"name": "John Doe", "email": "john@example.com", "content": ""})
         assert response.status_code == 400
         html = response.get_data(as_text=True)
         assert "Invalid content" in html
