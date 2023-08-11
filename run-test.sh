4237dfd7f7c40933241299112891dcecda7cb54d
Rami Elsayed <ramielsayed965@gmail.com>
2023-08-11 13:06:25 -0500

    Fixed Run Test Workflow; Linux (again)

--- run-test.sh
+++ run-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # note: it's bin for macos/linux and Scripts for windows
-$PWD/python3-virtualenv/Scripts/python -m unittest discover -v tests/
+$PWD/python3-virtualenv/bin/python -m unittest discover -v tests/
